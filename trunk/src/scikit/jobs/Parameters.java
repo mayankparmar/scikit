87332c8ea99ac9e6f5b07b177d1a05a6e9a28c2e
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2005-12-02 04:52:47 +0000

    First stab at more general parameters.

--- trunk/src/scikit/jobs/Parameters.java
+++ trunk/src/scikit/jobs/Parameters.java
@@ -5,9 +5,92 @@ import java.util.Vector;
 import java.util.HashMap;
 
 
+class Value {
+	protected String _v;
+	
+	public Value(String v) {
+		_v = v;
+	}
+	
+	public String sget() {
+		return _v;
+	}
+	
+	public int iget() {
+		throw new IllegalArgumentException();
+	}
+	
+	public double fget() {
+		throw new IllegalArgumentException();	
+	}
+	
+	public void set(String v) {
+		if (isValid(v))
+			_v = v;
+	}
+	
+	public boolean isValid(String v) {
+		return true;
+	}
+}
+
+
+class IntValue extends Value {
+	private int lo = Integer.MIN_VALUE, hi = Integer.MAX_VALUE;
+	
+	public IntValue(String v) {
+		super(v);
+	}
+	
+	public int iget() {
+		return Integer.valueOf(_v);
+	}
+	
+	public boolean isValid(String v) {
+		try {
+			int i = Integer.valueOf(v);
+			return lo < i && i < hi;
+		}
+		catch (NumberFormatException e) {
+			return false;
+		}
+	}
+}
+
+
+class DoubleValue extends Value {
+	private double _lo = Double.NEGATIVE_INFINITY, _hi = Double.POSITIVE_INFINITY;
+	
+	public DoubleValue(String v) {
+		super(v);
+	}
+	
+	public DoubleValue(String v, double lo, double hi) {
+		super(v);
+		_lo = lo;
+		_hi = hi;
+	}
+	
+	public double fget() {
+		return Double.valueOf(_v);
+	}
+	
+	public boolean isValid(String v) {
+		try {
+			double f = Double.valueOf(v);
+			return _lo < f && f < _hi;
+		}
+		catch (NumberFormatException e) {
+			return false;
+		}
+	}
+}
+
+
+
 public class Parameters implements Cloneable {
 	Vector<String> keys = new Vector<String>();
-	HashMap<String, Object> map = new HashMap<String, Object>();
+	HashMap<String, Value> map = new HashMap<String, Value>();
 	
 	
 	synchronized public String[] keys() {
@@ -16,74 +99,44 @@ public class Parameters implements Cloneable {
 	
 	public void add(String key, int value) {
 		keys.add(key);
-		put(key, value);		
+		put(key, new IntValue(String.valueOf(value)));		
 	}
 	
 	public void add(String key, double value) {
 		keys.add(key);
-		put(key, value);		
+		put(key, new DoubleValue(String.valueOf(value)));		
 	}
 	
 	public void add(String key, String value) {
 		keys.add(key);
-		put(key, value);		
+		put(key, new Value(value));		
 	}
 	
 	public void set(String key, String value) {
-		Object v = get(key);
-		if		(v instanceof String)	put(key, value);
-		else if (v instanceof Integer)	put(key, Integer.valueOf(value));
-		else if (v instanceof Double)	put(key, Double.valueOf(value));
-		else
-			throw new IllegalArgumentException("Parameter '"+key+"' does not exist.");
-	}
-	
-	public void set(String key, double value) {
-		Object v = get(key);
-		if		(v instanceof String)	put(key, ((Double)value).toString());
-		else if (v instanceof Integer)	put(key, (int)value);
-		else if (v instanceof Double)	put(key, value);
-		else
-			throw new IllegalArgumentException("Parameter '"+key+"' does not exist.");
+		get(key).set(value);
 	}
 	
 	public boolean isValidValue(String key, String value) {
-		Object v = get(key);
-		try {
-			if (v instanceof Double)	Double.valueOf(value);
-			if (v instanceof Integer)	Integer.valueOf(value);
-			return true;
-		}
-		catch (NumberFormatException e) {
-			return false;
-		}
+		return get(key).isValid(value);
 	}
 	
 	public double fget(String key) {
-		try {
-			return ((Double) get(key)).doubleValue();
-		} catch (ClassCastException e) {
-			throw new IllegalArgumentException("Parameter '"+key+"' is not a double.");
-		}
+		return get(key).fget();
 	}
 	
 	public int iget(String key) {
-		try {
-			return ((Integer) get(key)).intValue();
-		} catch (ClassCastException e) {
-			throw new IllegalArgumentException("Parameter '"+key+"' is not an integer.");
-		}
+		return get(key).iget();
 	}
 	
 	public String sget(String key) {
-		return get(key).toString();
+		return get(key).sget();
 	}
 	
 	public Object clone() {
 		try {
 			Parameters c = (Parameters) super.clone();
 			c.keys = (Vector<String>) keys.clone();
-			c.map = (HashMap<String, Object>) map.clone();
+			c.map = (HashMap<String, Value>) map.clone();
 			return c;
 		 } catch (CloneNotSupportedException e) {
 			return null;
@@ -91,11 +144,11 @@ public class Parameters implements Cloneable {
 	}
 	
 
-	synchronized private void put(String key, Object value) {
+	synchronized private void put(String key, Value value) {
 		map.put(key, value);
 	}
 	
-	synchronized private Object get(String key) {
+	synchronized private Value get(String key) {
 		if (!map.containsKey(key))
 			throw new IllegalArgumentException("Parameter '"+key+"' does not exist.");
 		return map.get(key);
