8660ccfdfc3df5f3a32c30319a00b8f080622754
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2006-08-03 15:19:42 +0000

    larger range for ordering app, correct theory.

--- src/kip/dyn1D/OrderingApp.java
+++ src/kip/dyn1D/OrderingApp.java
@@ -20,7 +20,7 @@ class Structure {
 	
 	public Structure(int _N, int numSteps) {
 		N = _N;
-		SN = N / 10;
+		SN = N / 40;
 		
 		theory = new double[SN];
 		avg = new double[SN];
@@ -38,21 +38,14 @@ class Structure {
 		fft.transform(data);
 		
 		cnt[timeIndex]++;
+		
+		accum[timeIndex][0] += data[0]*data[0] / N;
+		avg[0] = accum[timeIndex][0] / cnt[timeIndex];
 		for (int i = 1; i < SN; i++) {
 			accum[timeIndex][i] += (data[i]*data[i] + data[N-i]*data[N-i]) / N;
 			avg[i] = accum[timeIndex][i] / cnt[timeIndex];
 		}
-		
-/*		
-		// data[0] = data[0]*data[0];
-		data[0] = 0;
-		for (int i = 1; i < N/2; i++) {
-			data[i] = (data[i]*data[i] + data[N-i]*data[N-i]) / N;
-		}
-		for (int i = N/2; i < N; i++) {
-			data[i] = 0;
-		}
-*/		
+
 		coarse.updateAll();
 		return avg;
 	}
@@ -61,9 +54,10 @@ class Structure {
 		for (int i = 0; i < SN; i++) {
 			double k = 1e-10 + 2.0 * PI * i / N;
 			double D = sin(k*R)/(T*k*R) - 1;
-			double gamma = 2 - 4*t;
-			double A = gamma * exp(-2*t*D) / (2*D);
-			theory[i] = exp(2*t*D)*(1+A)-A;
+//			double gamma = 2 - 4*t;
+//			exp(2*t*D) + (gamma / (2*D)) * (1 - exp(-2*t*D));
+			double gamma = 2;
+			theory[i] = exp(2*t*D) + (gamma / (2*D)) * (-1 + exp(2*t*D));
 		}
 		return theory;
 	}
@@ -75,7 +69,7 @@ public class OrderingApp extends Job {
 	Plot structurePlot = new Plot("Structure", true);
 	Ising sim;
 	Structure structure;
-	int numSteps = 8;
+	int numSteps = 10;
 	
 	public static void main(String[] args) {
 		frame(new Control(new OrderingApp()), "Growth for Ising Droplets");
@@ -84,12 +78,12 @@ public class OrderingApp extends Job {
 	public OrderingApp() {
 		params.add("Memory time", 20.0, true);
 		params.add("Random seed", 0, true);
-		params.add("Coarse graining size", 1000.0, false);
+		params.add("Coarse graining size", 200.0, false);
 		params.add("N", 1<<20, true);
-		params.add("R", 128, true);
+		params.add("R", 512, true);
 		params.add("T", 4.0/9.0, false);
 		params.add("h", 0.0, false);
-		params.add("dt", 0.01, false);
+		params.add("dt", 0.2, false);
 		outputs.add("time");
 	}
 	
