d97ad886bd802a4f3c15f1761502659e3bd950db
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-01-03 23:51:43 +0000

    first steps toward intervention nucleation detection

--- src/kip/dyn1D/apps/NucleationApp.java
+++ src/kip/dyn1D/apps/NucleationApp.java
@@ -8,11 +8,12 @@ import scikit.jobs.*;
 
 public class NucleationApp extends Job {
 	Plot fieldPlot = new Plot("Fields", true);
+    Histogram profilePlot = new Histogram("Average Droplet Profile", 0.0, true);
 	Histogram nucTimes = new Histogram("Nucleation Times", 0.1, true);
+    
 	boolean phifour = true;
 	Dynamics1D sim;
 	
-	double equilibrationTime = 5;
 	// range of time for which to collect nucleating droplets
 	double lowBound, highBound;
 	// average difference between crude nucleation time, and real nucleation time
@@ -23,8 +24,7 @@ public class NucleationApp extends Job {
 	}
 	
 	public NucleationApp() {
-		
-		params.addm("Intervention overshoot", 10.0);
+		params.addm("Memory time", 20.0);
 		params.addm("Droplet low bound", 10000.0);
 		params.addm("Droplet high bound", 10000.0);
 		params.addm("Data path", "");
@@ -43,26 +43,36 @@ public class NucleationApp extends Job {
 	}
 	
 	public void animate() {
-		fieldPlot.setDataSet(0, new PointSet(0, sim.dx, sim.copyField()));
+		fieldPlot.setDataSet(0, new PointSet(0, (double)sim.dx/sim.R, sim.copyField()));
 		nucTimes.setBinWidth(2, params.fget("Bin width"));
 		sim.setParameters(params);
 	}
-	
+    
+    void accumulateDroplet(double[] field, double pos) {
+        int j = (int)round(pos/sim.dx);
+        int c = sim.N/sim.dx;
+        for (int i = 0; i < field.length; i++) {
+            profilePlot.accum(0, (double)i*sim.dx/sim.R, field[(i+j+c/2)%c]);
+        }
+    }
+    
 	void simulateUntilNucleation() {
 		while (!sim.nucleated() && sim.time() < highBound) {
 			sim.step();
 			yield();
 		}
 		if (sim.nucleated()) {
-			// if (lowBound < sim.t && sim.t < highBound)
-			//	droplet.findDroplet(oldoldsim, sim.t-overshootEstimate, sim.dropletLocation());
+			if (lowBound < sim.time() && sim.time() < highBound) {
+				double[] drop = sim.findNucleatingDroplet(overshootEstimate);
+                accumulateDroplet(sim.simulationAtTime(drop[0]).copyField(), drop[1]);
+            }
 			nucTimes.accum(2, sim.time());
 		}
 	}
 	
 	
 	public void run() {
-		overshootEstimate	= params.fget("Intervention overshoot");
+		overshootEstimate	= params.fget("Memory time");
 		lowBound			= params.fget("Droplet low bound");
 		highBound			= params.fget("Droplet high bound");		
 
@@ -71,11 +81,19 @@ public class NucleationApp extends Job {
 		}
 		sim.initialize(params);
 		
-		fieldPlot.setXRange(0, sim.N);
+		fieldPlot.setXRange(0, (double)sim.N/sim.R);
 		fieldPlot.setYRange(-0.7, 0.1);
+        fieldPlot.setDataSet(1, new Function(0, (double)sim.N/sim.R) {
+            public double eval(double kR) { return 0; }
+        });
+        
+        profilePlot.setBinWidth(1, (double)sim.dx/sim.R);
+        profilePlot.setAveraging(0, true);
+        
 		addDisplay(fieldPlot);
 		addDisplay(nucTimes);
-		
+		addDisplay(profilePlot);
+        
 		while (true) {
 			sim.initialize(params);
 			simulateUntilNucleation();
