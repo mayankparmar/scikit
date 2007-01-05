84fff75737643f01db66c43f0b44da9113afc306
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-01-05 00:40:37 +0000

    intervention method now seems to work.

--- src/kip/dyn1D/Dynamics1D.java
+++ src/kip/dyn1D/Dynamics1D.java
@@ -13,7 +13,11 @@ abstract public class Dynamics1D implements Cloneable {
 	public int N, R, dx;
 	public double time, dt;
 	
-	
+	static final double NUCLEATION_CUTOFF = 0;
+    static final double NUCLEATION_TIME_ACCURACY = 0.05;
+    static final double NUCLEATION_WAIT_TIME = 40;
+    
+    
 	public Dynamics1D clone() {
         try {
             Dynamics1D c = (Dynamics1D)super.clone();
@@ -92,22 +96,21 @@ abstract public class Dynamics1D implements Cloneable {
 	}
 	
 	
-	public boolean nucleated() {
-		for (int i = 0; i < N/dx; i++)
-			if (fieldElement(i) > 0)
-				return true;
-		return false;
-	}
-	
-	public double[] findNucleatingDroplet(double overshootEstimate) {
-        return new double[] {time(), dx*maxIndex(copyField())};
-    }
-    
 	abstract public double magnetization();
 	abstract public void randomizeField(double m);
 	abstract public double fieldElement(int i);
 	abstract protected void _step(); // step without saving "old" sim copies
     
+    
+    // -- NUCLEATION ----------------------------------------------------------------
+    //
+    
+	public boolean nucleated() {
+		for (int i = 0; i < N/dx; i++)
+			if (fieldElement(i) > NUCLEATION_CUTOFF)
+				return true;
+		return false;
+	}
     
     public static int maxIndex(double[] a) {
         int imax = 0;
@@ -115,6 +118,34 @@ abstract public class Dynamics1D implements Cloneable {
 			imax = a[i] > a[imax] ? i : imax;
         return imax;
     }
+    
+    // return time and location
+	public double[] nucleationTimeAndLocation(double overshootEstimate) {
+        double[] a = null;
+        double loc = dx*maxIndex(copyField());
+        
+        double t_hi = time();
+        double t_lo = max(time()-2*overshootEstimate, 0);
+        double t_md = (t_hi + t_lo) / 2;
+        
+        // binary search for nucleation time
+        while (t_hi - t_lo > NUCLEATION_TIME_ACCURACY) {
+            a = simulationAtTime(t_md).nucleationProbabilityAndLocation(loc);
+            if (a[0] < 0.5) {
+                t_lo = t_md;
+            }
+            else {
+                t_hi = t_md;
+                loc = a[1];
+            }
+            t_md = (t_hi + t_lo) / 2;
+        }
+        System.out.println(time() - t_md);
+        return new double[] {t_md, loc};
+    }
+    
+    
+    public double[] nucleationProbabilityAndLocation(double loc) {
+        return new double[] {0, loc};
+    }
 }
-
-
