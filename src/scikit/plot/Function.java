1d36b751db2cfe8926df0264d3a781f3722c53bd
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-01-26 20:54:29 +0000

    function class a little smarter about bounds, and exporting data.

--- src/scikit/plot/Function.java
+++ src/scikit/plot/Function.java
@@ -3,22 +3,28 @@ package scikit.plot;
 import static java.lang.Math.*;
 
 abstract public class Function extends DataSet {
-	double xmin, xmax;
-	int N = 100;
+	double xmin, xmax, ymin, ymax;
+	int N = 1024;
 	
 	public Function(double _xmin, double _xmax) {
 		xmin = _xmin;
 		xmax = _xmax;
+        ymin = ymax = eval(xmin);
+        for (double x = xmin; x < xmax; x += (xmax-xmin)/100) {
+            ymin = min(ymin, eval(x));
+            ymax = max(ymax, eval(x));
+        }
 	}
-	
+    
+    public double[] getBounds() {
+        return new double[] {xmin, xmax, ymin, ymax};
+    }
+    
 	public double[] copyData() {
-		return copyPartial(N, xmin, xmax, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+		return copyPartial(N, xmin, xmax, ymin, ymax);
 	}
 	
 	public double[] copyPartial(int N, double xmin, double xmax, double ymin, double ymax) {
-		xmin = max(xmin, this.xmin);
-		xmax = min(xmax, this.xmax);
-		
 		double[] ret = new double[2*N];
 		for (int i = 0; i < N; i++) {
 			double x = (xmax - xmin) * i / (N-1) + xmin;
