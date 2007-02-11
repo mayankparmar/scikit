90013a781164b7fd74ac10d2c68919d470c8db68
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-02-11 23:36:01 +0000

    ClumpApp with QuadTree working!

--- trunk/src/kip/clump/Clump2DApp.java
+++ trunk/src/kip/clump/Clump2DApp.java
@@ -3,50 +3,19 @@ package kip.clump;
 import kip.util.Random;
 import scikit.jobs.Control;
 import scikit.jobs.Job;
-import scikit.plot.DataSet;
 import scikit.plot.GridDisplay;
-import scikit.plot.Plot;
-import scikit.plot.PointSet;
 import static java.lang.Math.*;
-import static kip.util.MathPlus.*;
-
-
-class CoarseGrained {
-	double max;
-	int L;
-	double[] gridData;
-	
-	public CoarseGrained(double max, int L) {
-		this.max = max;
-		this.L = L;
-		gridData = new double[L*L];
-	}
-	
-	int pointToIndex(double x, double y) {
-		int i = (int) (L*x/max);
-		int j = (int) (L*y/max);
-		assert (i < L && j < L);
-		return j*L+i;
-	}
-	
-	public void addPoint(double x, double y) {
-		gridData[pointToIndex(x,y)] += 1;
-	}
-	
-	public void removePoint(double x, double y) {
-		gridData[pointToIndex(x,y)] -= 1;
-	}
-}
+import static java.lang.Integer.*;
 
 
 public class Clump2DApp extends Job {
     GridDisplay grid = new GridDisplay("Grid", true);
-    CoarseGrained cg;
+    QuadTree qt;
     int L, R;
-	double T;
+	double T, dx;
 	
 	int numPts;
-	double[] ptsX, ptsY;
+	int[] ptsX, ptsY;
 	Random random = new Random();
 	
 	
@@ -55,73 +24,72 @@ public class Clump2DApp extends Job {
 	}
 
 	public Clump2DApp() {
-		params.add("R", 10);
-		params.add("L", 100);
+		params.add("R", 16);
+		params.add("L/R", 16);
+		params.add("R/dx", 8);
 		params.addm("T", 1.0);
-		params.add("R/dx", 4);
 		params.add("Random seed", 0);
 	}
 
 	public void animate() {
-		T = params.fget("T");
+		T = params.fget("T")*dx*dx;
 	}
 	
 	void randomizePts() {
 		for (int i = 0; i < numPts; i++) {
-			ptsX[i] = random.nextDouble()*L;
-			ptsY[i] = random.nextDouble()*L;
-			cg.addPoint(ptsX[i], ptsY[i]);
+			ptsX[i] = (int) (random.nextDouble()*L);
+			ptsY[i] = (int) (random.nextDouble()*L);
+			qt.add(ptsX[i], ptsY[i]);
 		}
-	}
-	
-	int countInteractions(double x, double y) {
-		int acc = 0;
-		for (int i = 0; i < numPts; i++) {
-			double dx = abs(x-ptsX[i]);
-			double dy = abs(y-ptsY[i]);
-			if (dx > L/2) dx = L - dx;
-			if (dy > L/2) dy = L - dy;
-			if (dx*dx+dy*dy < R*R) acc++;
-		}
-		return acc;
 	}
 	
 	void simStep() {
 		int i = (int) (numPts*random.nextDouble());
-		double x = ptsX[i];
-		double y = ptsY[i];
+		int x = ptsX[i];
+		int y = ptsY[i];
 		
-		double xp = x + R*2*(random.nextDouble()-0.5);
-		double yp = y + R*2*(random.nextDouble()-0.5);
+		int xp = x + (int)(R*2*(random.nextDouble()-0.5));
+		int yp = y + (int)(R*2*(random.nextDouble()-0.5));
 		xp = (xp+L)%L;
 		yp = (yp+L)%L;
 		
-		double dE = (countInteractions(xp,yp)-countInteractions(x,y))/(PI*R*R);
+		double dE = (qt.countOverlaps(xp,yp)-qt.countOverlaps(x,y))/(PI*R*R);
 		if (dE < 0 || random.nextDouble() < exp(-dE/T)) {
 			ptsX[i] = xp;
 			ptsY[i] = yp;
-			cg.removePoint(x, y);
-			cg.addPoint(xp, yp);
+			qt.remove(x, y);
+			qt.add(xp, yp);
 		}
 	}
 	
 	
 	public void run() {
-		L = params.iget("L");
-		R = params.iget("R");
-		T = params.fget("T");
 		random.setSeed(params.iget("Random seed", 0));
-		
+
+		R = params.iget("R");
+		L = R*params.iget("L/R");
         numPts = L*L;
-		ptsX = new double[numPts];
-		ptsY = new double[numPts];
- 
-        int cgL = L*params.iget("R/dx")/R;		// # coarse grained cells per side
-		cg = new CoarseGrained(L, cgL);
-        grid.setData(cgL, cgL, cg.gridData);
+        
+		// adjust dx so that L/dx is a power of two
+		dx = R/params.iget("R/dx");
+		// int Lp = highestOneBit((int)round(L/dx));
+		//dx = (double)L/Lp;
+		//params.set("dx/R", dx/R);
+		
+		// rescale L,R,T so that dx->1 for simplicity
+		L = (int)(L/dx);
+		R = (int)(R/dx);
+		T = params.fget("T")*dx*dx;
+		if (L != highestOneBit(L))
+			throw new IllegalArgumentException("L/dx must be a power of 2.");
+		
+		qt = new QuadTree(L, R);
+        grid.setData(L, L, qt.rawElements);
         addDisplay(grid);
         
-        randomizePts();
+		ptsX = new int[numPts];
+		ptsY = new int[numPts];
+		randomizePts();
         
         while (true) {
 			yield();
