d442b8709b671eb761acfa8b11b14fb2a9c3caaa
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-02-13 01:20:24 +0000

    structure factor calculation now matches data for Clump model!

--- trunk/src/kip/clump/StructureFactor.java
+++ trunk/src/kip/clump/StructureFactor.java
@@ -7,23 +7,40 @@ import static java.lang.Math.*;
 /*
 * Calculates the structure factor
 */
-class StructureFactor {
+public class StructureFactor {
 	ComplexDouble2DFFT fft;	// Object to perform transforms
 	double[] fftData;       // Fourier transform data
-	int L;
+	int Lp;                 // # elements per side
+	double L;               // the actual system length, L = Lp*dx, where dx is lattice spacing
+	double R;               // characteristic length.  x-axis is k*R.
+	double kRmin, kRmax;
 	Accumulator acc;
 	
-	StructureFactor(int L, double binWidth) {
+	public StructureFactor(int Lp, double L, double R, double kRbinWidth) {
+		this.Lp = Lp;
 		this.L = L;
-		acc = new Accumulator(binWidth);
+		this.R = R;
+		
+		kRmin = (2*PI*2/L)*R; // explicitly exclude constant (k=0) mode
+		kRmax = (2*PI*(Lp/2)/L)*R;
+		acc = new Accumulator(kRbinWidth*(L/R)/(2*PI));
 		acc.setAveraging(true);
-		fft = new ComplexDouble2DFFT(L, L);
-		fftData = new double[2*L*L];
+		fft = new ComplexDouble2DFFT(Lp, Lp);
+		fftData = new double[2*Lp*Lp];
 	}
-
-	Accumulator calculate(int[] data) {
+	
+	public Accumulator getAccumulator() {
+		return acc;
+	}
+	
+	public void setBounds(double kRmin, double kRmax) {
+		this.kRmin = kRmin;
+		this.kRmax = kRmax;
+	}
+	
+	void accumulate(int[] data) {
 		// compute fourier transform
-		for (int i = 0; i < L*L; i++) {
+		for (int i = 0; i < Lp*Lp; i++) {
 			fftData[2*i] = data[i];
 			fftData[2*i+1] = 0;
 		}
@@ -31,25 +48,26 @@ class StructureFactor {
 		fftData = fft.toWraparoundOrder(fftData);
 		
 		// verify imaginary component of structure factor is zero
-		for (int y = -L/2; y < L/2; y++) {
-			for (int x = -L/2; x < L/2; x++) {
-				int i = L*((y+L)%L) + (x+L)%L;
-				int j = L*((-y+L)%L) + (-x+L)%L;
+		for (int y = -Lp/2; y < Lp/2; y++) {
+			for (int x = -Lp/2; x < Lp/2; x++) {
+				int i = Lp*((y+Lp)%Lp) + (x+Lp)%Lp;
+				int j = Lp*((-y+Lp)%Lp) + (-x+Lp)%Lp;
 				assert(abs(fftData[2*i+1] + fftData[2*j+1]) < 1e-11);
 			}
 		}
 		
 		// We calculate the structure factor s(k) by summing the fourier transform information
 		// over all frequencies with equal magnitude (k).
-		for (int y = -L/2; y < L/2; y++) {
-			for (int x = -L/2; x < L/2; x++) {
-				double k = sqrt(x*x+y*y);
-				int i = L*((y+L)%L) + (x+L)%L;
-				double re = fftData[2*i];
-				double im = fftData[2*i+1];
-				acc.accum(k, (re*re + im*im)/(L*L));
+		for (int y = -Lp/2; y < Lp/2; y++) {
+			for (int x = -Lp/2; x < Lp/2; x++) {
+				double kR = (2*PI*sqrt(x*x+y*y)/L)*R;
+				if (kR >= kRmin && kR <= kRmax) {
+					int i = Lp*((y+Lp)%Lp) + (x+Lp)%Lp;
+					double re = fftData[2*i];
+					double im = fftData[2*i+1];
+					acc.accum(kR, (re*re + im*im)/(L*L));
+				}
 			}
 		}
-		return acc;
 	}
 }
