825c8c3e5f2639a6661d0772d123d4ea3eb1145c
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-02-13 01:20:24 +0000

    structure factor calculation now matches data for Clump model!

--- src/kip/clump/Clump2DApp.java
+++ src/kip/clump/Clump2DApp.java
@@ -1,10 +1,12 @@
 package kip.clump;
 
 import kip.util.Random;
+import static kip.util.MathPlus.*;
 import scikit.jobs.Control;
 import scikit.jobs.Job;
 import scikit.plot.GridDisplay;
 import scikit.plot.Plot;
+import scikit.plot.Function;
 import static java.lang.Math.*;
 import static java.lang.Integer.*;
 
@@ -16,8 +18,12 @@ public class Clump2DApp extends Job {
     QuadTree qt;
     StructureFactor sf;
     
-    int L, R;
-	double T, dx;
+    // unscaled parameters
+    int L, R, dx;
+    double T;
+	// parameters with dx scaled out, for implementation purposes
+	int Lp, Rp;
+	double Tp;
 	
 	int numPts;
 	int[] ptsX, ptsY;
@@ -32,18 +38,22 @@ public class Clump2DApp extends Job {
 		params.add("R", 16);
 		params.add("L/R", 16);
 		params.add("R/dx", 8);
-		params.addm("T", 0.14);
+		params.addm("T", 1.0);
+		params.addm("kR bin-width", 0.02);
 		params.add("Random seed", 0);
+		params.add("Time");
 	}
-
+	
 	public void animate() {
-		T = params.fget("T")*dx*dx;
+		T = params.fget("T");
+		Tp = T*dx*dx;
+		sf.getAccumulator().setBinWidth(params.fget("kR bin-width"));
 	}
 	
 	void randomizePts() {
 		for (int i = 0; i < numPts; i++) {
-			ptsX[i] = (int) (random.nextDouble()*L);
-			ptsY[i] = (int) (random.nextDouble()*L);
+			ptsX[i] = (int) (random.nextDouble()*Lp);
+			ptsY[i] = (int) (random.nextDouble()*Lp);
 			qt.add(ptsX[i], ptsY[i]);
 		}
 	}
@@ -53,13 +63,13 @@ public class Clump2DApp extends Job {
 		int x = ptsX[i];
 		int y = ptsY[i];
 		
-		int xp = x + (int)(R*2*(random.nextDouble()-0.5));
-		int yp = y + (int)(R*2*(random.nextDouble()-0.5));
-		xp = (xp+L)%L;
-		yp = (yp+L)%L;
+		int xp = x + (int)(Rp*2*(random.nextDouble()-0.5));
+		int yp = y + (int)(Rp*2*(random.nextDouble()-0.5));
+		xp = (xp+Lp)%Lp;
+		yp = (yp+Lp)%Lp;
 		
-		double dE = (qt.countOverlaps(xp,yp)-qt.countOverlaps(x,y))/(PI*R*R);
-		if (dE < 0 || random.nextDouble() < exp(-dE/T)) {
+		double dE = (qt.countOverlaps(xp,yp)-qt.countOverlaps(x,y))/(PI*Rp*Rp);
+		if (dE < 0 || random.nextDouble() < exp(-dE/Tp)) {
 			ptsX[i] = xp;
 			ptsY[i] = yp;
 			qt.remove(x, y);
@@ -73,39 +83,44 @@ public class Clump2DApp extends Job {
 
 		R = params.iget("R");
 		L = R*params.iget("L/R");
-        numPts = L*L;
-        
-		// adjust dx so that L/dx is a power of two
 		dx = R/params.iget("R/dx");
-		// int Lp = highestOneBit((int)round(L/dx));
-		//dx = (double)L/Lp;
-		//params.set("dx/R", dx/R);
-		
+		T = params.fget("T");
+        numPts = L*L;
+        if (highestOneBit(R)!=R || highestOneBit(L)!=L || highestOneBit(dx)!=dx)
+			throw new IllegalArgumentException("All parameters must be powers of 2.");
+        
 		// rescale L,R,T so that dx->1 for simplicity
-		L = (int)(L/dx);
-		R = (int)(R/dx);
-		T = params.fget("T")*dx*dx;
-		if (L != highestOneBit(L))
-			throw new IllegalArgumentException("L/dx must be a power of 2.");
+		Lp = L/dx;
+		Rp = R/dx;
+		Tp = T*dx*dx;
 		
-		qt = new QuadTree(L, R);
-        grid.setData(L, L, qt.rawElements);
+		qt = new QuadTree(Lp, Rp);
+        grid.setData(Lp, Lp, qt.rawElements);
         addDisplay(grid);
         
 		ptsX = new int[numPts];
 		ptsY = new int[numPts];
 		randomizePts();
         
-		sf = new StructureFactor(L, 0.25);
-        plot.setDataSet(0, sf.calculate(qt.rawElements));
+		sf = new StructureFactor(Lp, L, R, params.fget("kR bin-width"));
+        plot.setDataSet(0, sf.getAccumulator());
+        plot.setDataSet(1, new Function(sf.kRmin, sf.kRmax) {
+        	public double eval(double kR) {
+        		double V = 2*j1(kR)/kR;
+        		return 1/(V/T+1);
+        	}
+        });
         addDisplay(plot);
         
+        double t = 0;
         while (true) {
-			for (int i = 0; i < numPts/100; i++) {
+			params.set("Time", t);
+			for (int i = 0; i < numPts/8; i++) {
 				simStep();
 				yield();
 			}
-			sf.calculate(qt.rawElements);
+			sf.accumulate(qt.rawElements);
+			t += 1./8.;
 		}
         // params.set("Random seed", params.iget("Random seed")+1);
 	}
