5aa9e42b46d285b61a986ca0cea23c21ffb7da54
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-05-30 18:34:21 +0000

    ising field simulations much improved.  can now initialize field to fixed value, or randomized values.

--- trunk/src/kip/dyn1D/FieldIsing.java
+++ trunk/src/kip/dyn1D/FieldIsing.java
@@ -1,11 +1,17 @@
 package kip.dyn1D;
 
+import jnt.FFT.ComplexDoubleFFT;
+import jnt.FFT.ComplexDoubleFFT_Mixed;
 import scikit.params.Parameters;
 import static java.lang.Math.*;
+import static kip.util.MathPlus.*;
 
 
 public class FieldIsing extends AbstractIsing {
-	public double[] field, scratch1, scratch2;
+	public double[] phi, phibar, phi2, phibar2;
+	ComplexDoubleFFT fft;	// Object to perform transforms
+	public double[] fftScratch;
+	int Lp;
 	
 	public FieldIsing(Parameters params) {
 		initialize(params);
@@ -14,7 +20,7 @@ public class FieldIsing extends AbstractIsing {
 	
     public FieldIsing clone() {
 		FieldIsing c = (FieldIsing)super.clone();
-		c.field = (double[])field.clone();
+		c.phi = (double[])phi.clone();
 		return c;
     }
 	
@@ -22,138 +28,130 @@ public class FieldIsing extends AbstractIsing {
 	// reset time, set random number seed, initialize fields to down
 	public void initialize(Parameters params) {
 		super.initialize(params);
-		field = new double[N/dx];
-		scratch1 = new double[N/dx];
-		scratch2 = new double[N/dx];		
+		Lp = N/dx;
+		phi = new double[Lp];
+		phibar = new double[Lp];
+		phi2 = new double[Lp];
+		phibar2 = new double[Lp];
+		
+		fftScratch = new double[2*Lp];
+		fft = new ComplexDoubleFFT_Mixed(Lp);		
 	}
 	
 	
 	public void randomizeField(double m) {
-		assert (m == 0);
-		
-		for (int i = 0; i < N/dx; i++) {
-			field[i] = random.nextGaussian() / sqrt(dx);
-		}
+		for (int i = 0; i < Lp; i++)
+			phi[i] = m + random.nextGaussian()*sqrt((1-m*m)/dx);
 	}
 	
+	public void setField(double m) {
+		for (int i = 0; i < Lp; i++)
+			phi[i] = m;
+	}
 	
 	public double magnetization() {
 		double sum = 0;
-		for (int i = 0; i < N/dx; i++)
-			sum += field[i];
-		return sum / (N/dx);
+		for (int i = 0; i < Lp; i++)
+			sum += phi[i];
+		return sum / Lp;
 	}
 	
 	
 	public double fieldElement(int i) {
-		return field[i];
+		return phi[i];
 	}
 	
 	
-	private double bar(double[] field, int i) {
-		int nblocks = (int)round(2.0*R/dx);
-		
-		double acc = field[i];
-		for (int j = 1; j <= nblocks/2; j++) {
-			acc += field[(i+j)%(N/dx)];
-			acc += field[(i-j+N/dx)%(N/dx)];
+	private void convolveWithRange(double[] src, double[] dest) {
+		for (int i = 0; i < Lp; i++) {
+			fftScratch[2*i] = src[i];
+			fftScratch[2*i+1] = 0;
 		}
-		return acc / (1 + 2*(nblocks/2));
+		
+		fft.transform(fftScratch);
+		for (int x = -Lp/2; x < Lp/2; x++) {
+				double kR = (2*PI*x/N) * R;
+				int i = (x+Lp)%Lp;
+				double J = (kR == 0 ? 1 : sin(kR)/kR);
+				fftScratch[2*i] *= J;
+				fftScratch[2*i+1] *= J;
+		}
+		fft.backtransform(fftScratch);
+		
+		for (int i = 0; i < Lp; i++) {
+			dest[i] = fftScratch[2*i] / Lp;
+		}		
 	}
 	
+	
+//	private double bar(double[] field, int i) {
+//		int nblocks = (int)round(2.0*R/dx);
+//		
+//		double acc = field[i];
+//		for (int j = 1; j <= nblocks/2; j++) {
+//			acc += field[(i+j)%Lp];
+//			acc += field[(i-j+Lp)%Lp];
+//		}
+//		return acc / (1 + 2*(nblocks/2));
+//	}
+	
+	private double drift(double phi, double phibar) {
+		phibar = (J*phibar+h)/T;
+		double tphi = tanh(phibar);
+		switch (dynamics) {
+		case GLAUBER:
+			return tphi - phi;
+		case METROPOLIS:
+			return 2*exp(-abs(phibar)) * (sinh(phibar) - phi*cosh(phibar));
+		default:
+			return Double.NaN;
+		}
+	}
+	
+	public double noise(double phi, double phibar) {
+		double tphi = tanh((J*phibar+h)/T);
+		switch (dynamics) {
+		case GLAUBER:
+			return sqrt(2 - tphi*tphi - phi*phi);
+		case METROPOLIS:
+			return 2; // ???
+		default:
+			return Double.NaN;
+		}
+	}
 	
 	protected void _step() {
 		// break update into "steps" number of substeps. each one with time interval dt_
 		double dx_ = dx;
-		int steps = (int) max(sqrt(dt) * 100 / sqrt(dx_), 1);
-		double dt_ = dt / steps;
+		double CONST = 100000;
+		int steps = (int) max(dt*CONST/(dx_*dx_), 1);
+		double dt_ = dt / steps; // = dx_/k
 		
-		double K = J / T;
-		double H = h / T;
-		
-		/*
-		// ----------------------------------------- linear theory
+		// full heun scheme
 		for (int cnt = 0; cnt < steps; cnt++) {
-			for (int i = 0; i < N/dx; i++) {
-				double f = field[i];
-				double g = K*bar(field, i) + H;
-				double U = g - f;
-				double V = sqrt(2);
-				double eta = random.nextGaussian();
-				scratch1[i] = f + dt_*U + sqrt(dt_/dx_)*eta*V;
-			}
-			for (int i = 0; i < N/dx; i++) {
-				field[i] = scratch1[i];
-			}
-		}
-		*/
-		
-		// ----------------------------------------- heun scheme for perturbation in phi^2
-		for (int cnt = 0; cnt < steps; cnt++) {
+			convolveWithRange(phi, phibar);
+			
 			// get euler predictor
 			for (int i = 0; i < N/dx; i++) {
-				double f = field[i];
-				double g = K*bar(field, i) + H;
-				double U = g - (g*g*g/3) - f;
-				double V = sqrt(2) * (1 /* - sqr(g)/4 - sqr(f)/4 */);
+				if (sqr(phi[i]) > 1)
+					throw new ArithmeticException();
+				double U = drift(phi[i], phibar[i]);
+				double V = noise(phi[i], phibar[i]);
 				double eta = random.nextGaussian();
-				scratch1[i] = f + dt_*U + sqrt(dt_/dx_)*eta*V;
+				phi2[i] = phi[i] + dt_*U + sqrt(dt_/dx_)*eta*V;
 			}
+			
+			convolveWithRange(phi2, phibar2);
 			
 			// take step based on predictor
 			for (int i = 0; i < N/dx; i++) {
-				double f1 = field[i];
-				double f2 = scratch1[i];
-				double g1 = K*bar(field, i) + H;
-				double g2 = K*bar(scratch1, i) + H;
-				double U1 = g1 - (g1*g1*g1/3) - f1;
-				double U2 = g2 - (g2*g2*g2/3) - f2;
-				double V1 = sqrt(2) * (1 /* - sqr(g1)/4 - sqr(f1)/4 */);
-				double V2 = sqrt(2) * (1 /* - sqr(g2)/4 - sqr(f2)/4 */);
-				
+				double U1 = drift(phi[i],  phibar[i]);
+				double U2 = drift(phi2[i], phibar2[i]);
+				double V1 = noise(phi[i],  phibar[i]);
+				double V2 = noise(phi2[i], phibar2[i]);
 				double eta = random.nextGaussian();
-				scratch2[i] = f1 + dt_*(U1+U2)/2 + sqrt(dt_/dx_)*eta*(V1+V2)/2;
-			}
-			
-			// copy back to field
-			for (int i = 0; i < N/dx; i++) {
-				field[i] = scratch2[i];
+				phi[i] += dt_*(U1+U2)/2 + sqrt(dt_/dx_)*eta*(V1+V2)/2;
 			}
 		}
-		
-		/*
-		// ----------------------------------------- full heun scheme
-		for (int cnt = 0; cnt < steps; cnt++) {
-			// get euler predictor
-			for (int i = 0; i < N/dx; i++) {
-				double f = field[i];
-				double g = K*bar(field, i) + H;
-				double U = tanh(g) - f;
-				double V = sqrt(2 - sqr(tanh(g)) - sqr(f));
-				double eta = random.nextGaussian();
-				scratch1[i] = f + dt_*U + sqrt(dt_/dx_)*eta*V;
-			}
-			
-			// take step based on predictor
-			for (int i = 0; i < N/dx; i++) {
-				double f1 = field[i];
-				double f2 = scratch1[i];
-				double g1 = K*bar(field, i) + H;
-				double g2 = K*bar(scratch1, i) + H;
-				double U1 = tanh(g1) - f1;
-				double U2 = tanh(g2) - f2;
-				double V1 = sqrt(2 - sqr(tanh(g1)) - sqr(f1));
-				double V2 = sqrt(2 - sqr(tanh(g2)) - sqr(f2));
-				
-				double eta = random.nextGaussian();
-				scratch2[i] = f1 + dt_*(U1+U2)/2 + sqrt(dt_/dx_)*eta*(V1+V2)/2;
-			}
-			
-			// copy back to field
-			for (int i = 0; i < N/dx; i++) {
-				field[i] = scratch2[i];
-			}
-		}
-		*/
 	}
 }
