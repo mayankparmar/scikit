edbe0b6a04717ce76b1fecb08b4cf8f9a8ac3b5b
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-06-26 20:16:59 +0000

    Fixed thread race condition in Cooperation/Job. Was only showing up in Linux and Windows.

--- src/scikit/jobs/Cooperation.java
+++ src/scikit/jobs/Cooperation.java
@@ -6,6 +6,12 @@ import javax.swing.SwingUtilities;
 public class Cooperation {
 	private volatile boolean triggered = false;
 	
+	// adds an event to the GUI event queue which, when called back, will
+	// call pass() from the GUI thread. pass() will hang the GUI thread
+	// and allow the simulation thread to run. when the simulation thread
+	// calls pass(), the GUI thread will back awakened, and this callback
+	// will return.
+	// in summary, schedules one processing loop while the GUI thread waits
 	public void triggerProcessingLoop() {
 		if (!triggered) {
 			triggered = true;
@@ -18,14 +24,20 @@ public class Cooperation {
 		}
 	}
 	
+	// adds this thread to the processing loop
 	public void register() {
+		// make sure that this thread is being run cooperatively with the GUI thread
+		// (the GUI thread should hang while this thread is being processing)
+		triggerProcessingLoop();
 		pass();
 	}
 	
+	// removes this thread from the processing loop
 	synchronized public void unregister() {
 		notify();
 	}
 	
+	// cooperatively allow the other thread in the processing loop to run
 	synchronized public void pass() {
 		notify();
 		try {
