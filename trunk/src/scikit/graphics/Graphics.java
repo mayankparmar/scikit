464882d2ee0bdc2a200f75745b2fb0fbbe46d715
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-07-16 16:08:50 +0000

    Somehow the new Graphics Interface didn't get committed last time. Now everything should compile and work.

--- trunk/src/scikit/graphics/Graphics.java
+++ trunk/src/scikit/graphics/Graphics.java
@@ -1,10 +1,23 @@
 package scikit.graphics;
 
-import javax.media.opengl.GL;
+import java.awt.Color;
 
 import scikit.util.Bounds;
 
+
 public interface Graphics {
-	public void draw(GL gl, Bounds bounds);
-	public Bounds getBounds();
+	public Object engine();
+	public Scene scene();
+	public void projectOrtho2D(Bounds bds);
+	public void setColor(Color color);
+	public void drawPoint(double x, double y);
+	public void drawLine(double x1, double y1, double x2, double y2);
+	public void drawLines(double[] xys);
+	public void drawRect(double x, double y, double w, double h);
+	public void fillRect(double x, double y, double w, double h);
+	public void drawCircle(double x, double y, double r);
+	public void fillCircle(double x, double y, double r);
+	public double stringWidth(String str);
+	public double stringHeight(String str);
+	public void drawString(String str, double x, double y);
 }
