8f9f68ac2cad9d8bcca67ea63b85ddc207fb3d16
erg.dominguez <erg.dominguez@508e2490-7634-0410-858c-2f298a935b86>
2007-07-18 02:35:05 +0000

    Structure factor added to 1D ising field simulation, but not working yet

--- src/rachele/ising/dim1/IsingField1DApp.java
+++ src/rachele/ising/dim1/IsingField1DApp.java
@@ -1,6 +1,8 @@
 package rachele.ising.dim1;
 
 
+import static java.lang.Math.floor;
+import static scikit.util.Utilities.format;
 import scikit.dataset.PointSet;
 import scikit.jobs.Control;
 import scikit.jobs.Job;
@@ -11,9 +13,9 @@ import rachele.ising.dim1.FieldIsing1D;
 public class IsingField1DApp extends Simulation{
 
 	Plot fieldPlot = new Plot("Coarse Grained Field", true);
-    //Plot plot = new Plot("Structure factor", true);
-    //StructureFactor sf;
+    Plot SFPlot = new Plot("Structure factor", true);
     FieldIsing1D ising;
+    StructureFactor1D sf;
 	
 	public static void main(String[] args) {
 		new Control(new IsingField1DApp(), "Ising Field 1D");
@@ -31,15 +33,33 @@ public class IsingField1DApp extends Simulation{
 		params.add("Time");
 	}
 	
+	public void animate() {
+		params.set("Time", format(ising.t));
+		//ising.setParameters(params);
+		
+		SFPlot.setDataSet(0, sf.getAccumulator());
+	
+		if (flags.contains("Clear S.F.")) {
+			sf.getAccumulator().clear();
+			System.out.println("clicked");
+		}
+		flags.clear();
+	}
+	
 	public void run(){
-		System.out.println("start");
 		ising = new FieldIsing1D(params);
+		double binWidth = ising.KR_SP / floor(ising.KR_SP/params.fget("kR bin-width"));
+		sf = new StructureFactor1D(ising.Lp, ising.L, ising.R, binWidth);
 		fieldPlot.setYRange(-1, 1);
 		Job.addDisplay(fieldPlot);
+		Job.addDisplay(SFPlot);
+		sf.getAccumulator().clear();
 		
 		while (true) {
 			params.set("Time", ising.time());
 			ising.simulate();
+			sf.accumulate(ising.phi);
+			//avStructH.accum(structure.getAccumulatorH());
 			Job.animate();
 			fieldPlot.setDataSet(0, new PointSet(0, ising.dx, ising.phi));
 		}
