a4866cf5c25edd8d67d33e492902a1dc388cd53a
erg.dominguez <erg.dominguez@508e2490-7634-0410-858c-2f298a935b86>
2007-08-05 15:43:05 +0000

    path sampling reads in real configuration, free energy plot added

--- src/rachele/ising/dim1/apps/SaddlePoint1DApp.java
+++ src/rachele/ising/dim1/apps/SaddlePoint1DApp.java
@@ -2,6 +2,10 @@ package rachele.ising.dim1.apps;
 
 import static java.lang.Math.floor;
 import static scikit.util.Utilities.format;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import rachele.ising.dim1.PathSample1D;
 import rachele.ising.dim1.StructureFactor1D;
 import scikit.jobs.Control;
@@ -19,6 +23,7 @@ public class SaddlePoint1DApp extends Simulation{
 	Plot timeSlice = new Plot("Configuration at t_f/2", true);
 	Plot spaceSlice = new Plot("Path at Lp/2", true);
     Plot actionPlot = new Plot("Action", true);
+    Plot freeEngPlot = new Plot("Free Energy", true);
 	PathSample1D sim;
     StructureFactor1D sf;
 	
@@ -29,21 +34,23 @@ public class SaddlePoint1DApp extends Simulation{
 
 	public SaddlePoint1DApp(){
 		params.addm("Sampling Noise", new ChoiceValue("On", "Off"));
-		params.addm("Init Step", new ChoiceValue("Off", "On"));
+		params.addm("Initial Conditions", new ChoiceValue("Read In", "Boundaries Only", "Step", "Constant Slope"));
 		params.addm("Time to slice", new DoubleValue(0.5, 0, 0.9999).withSlider());
 		params.addm("Position to slice", new DoubleValue(0.5, 0, 0.9999).withSlider());
+		params.addm("du", 0.004);
 		params.addm("T", 0.785);
 		params.addm("J", -1.0);
-		params.addm("dt", 0.1);
-		params.addm("R", 100000);
 		params.addm("H", 0.0);
+		params.addm("R", 100000.0);
 		params.add("L/R", 8.0);
 		params.add("R/dx", 8.0);
 		params.add("kR bin-width", 0.1);
 		params.add("Random seed", 0);
-		params.add("Density", -0.3);
-		params.addm("du", 0.004);
-		params.add("Time Interval", 300);
+		//params.add("Density", -0.3);
+		params.add("dt", 0.1);
+		params.add("Time Interval", 30.0);
+		params.add("Time Allocation");
+		params.add("Lp");
 		params.add("u");
 		params.add("action");
 		params.add("init denisty", -0.73);
@@ -56,6 +63,9 @@ public class SaddlePoint1DApp extends Simulation{
 		params.set("u", format(sim.u));
 		params.set("action", format(sim.S));
 		
+		sim.measureAction();
+
+		freeEngPlot.setDataSet(0, sim.measureFreeEnergy());
 		timeSlice.setDataSet(0, sim.getTimeSlice());
 		spaceSlice.setDataSet(0, sim.getSpaceSlice());
 		actionPlot.setDataSet(0, sim.getAccumulator());
@@ -71,8 +81,63 @@ public class SaddlePoint1DApp extends Simulation{
 		//flags.clear();
 	}
 	
+	public void readInputParams(String FileName){
+		try {
+			File inputFile = new File(FileName);
+			DataInputStream dis = new DataInputStream(new FileInputStream(inputFile));
+			double readData;
+			
+			readData = dis.readDouble();
+			System.out.println(readData);
+			params.set("T", readData);
+			dis.readChar();
+			
+			readData = dis.readDouble();
+			System.out.println(readData);
+			params.set("J", readData);
+			dis.readChar();				
+		
+			readData = dis.readDouble();
+			System.out.println(readData);
+			params.set("H", readData);
+			dis.readChar();
+			
+			readData = dis.readDouble();
+			System.out.println(readData);
+			params.set("R", readData);
+			dis.readChar();	
+			
+			readData = dis.readDouble();
+			System.out.println(readData);
+			params.set("L/R", readData);
+			dis.readChar();
+			
+			readData = dis.readDouble();
+			System.out.println(readData);
+			params.set("R/dx", readData);
+			dis.readChar();	
+			
+			readData = dis.readDouble();
+			System.out.println(readData);
+			params.set("dt", readData);
+			dis.readChar();
+			
+			readData = dis.readDouble();
+			System.out.println(readData);
+			params.set("Time Interval", readData);
+			dis.close();
+			System.out.println("input read");
+		}catch(IOException ex){
+			ex.printStackTrace();
+		}
+	}
+	
 	public void run(){
 
+		if(params.sget("Initial Conditions") == "Read In")
+			readInputParams("racheleDataFiles/inputParams");
+		if(params.sget("Initial Conditions") == "Boundaries Only")
+			readInputParams("racheleDataFiles/inputParams");		
 		sim = new PathSample1D(params);
 		double KR_SP = PathSample1D.KR_SP;
 		double binWidth = KR_SP / floor(KR_SP/params.fget("kR bin-width"));
@@ -81,6 +146,7 @@ public class SaddlePoint1DApp extends Simulation{
 		Job.addDisplay(timeSlice);
 		Job.addDisplay(spaceSlice);
 		Job.addDisplay(actionPlot);
+		Job.addDisplay(freeEngPlot);
 		timeSlice.setDataSet(0, sim.getTimeSlice());
 		spaceSlice.setDataSet(0, sim.getSpaceSlice());
 		actionPlot.setDataSet(0, sim.getAccumulator());
