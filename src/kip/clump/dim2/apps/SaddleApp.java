88efcdafbf9022b1b542d99f159b16627bb7e8f9
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-09-20 03:13:13 +0000

    display free energy density in saddle app.  fix calculation bug in fieldclump2d.

--- src/kip/clump/dim2/apps/SaddleApp.java
+++ src/kip/clump/dim2/apps/SaddleApp.java
@@ -1,12 +1,13 @@
 package kip.clump.dim2.apps;
 
+import static scikit.util.Utilities.format;
 import static scikit.util.Utilities.frame;
-import kip.clump.dim2.*;
+import kip.clump.dim2.FieldClump2D;
+import scikit.graphics.dim2.Grid;
 import scikit.jobs.Control;
 import scikit.jobs.Job;
 import scikit.jobs.Simulation;
 import scikit.params.ChoiceValue;
-import scikit.graphics.dim2.Grid;
 
 
 public class SaddleApp extends Simulation {
@@ -27,6 +28,7 @@ public class SaddleApp extends Simulation {
 		params.add("dx", 125.0);
 		params.add("Random seed", 0);
 		params.add("Time");
+		params.add("F density");
 		params.add("dF/dphi");
 		params.add("Valid profile");
 	}
@@ -37,8 +39,9 @@ public class SaddleApp extends Simulation {
 			grid.registerData(clump.numColumns(), clump.numColumns(), clump.coarseGrained());
 		else
 			grid.registerData(clump.numColumns(), clump.numColumns(), clump.coarseGrained(), 0, 2);
-		params.set("Time", clump.time());
-		params.set("dF/dphi", clump.rms_dF_dphi);
+		params.set("Time", format(clump.time()));
+		params.set("F density", format(clump.freeEnergyDensity));
+		params.set("dF/dphi", format(clump.rms_dF_dphi));
 		params.set("Valid profile", !clump.rescaleClipped);
 	}
 	
