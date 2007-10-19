d30b39af28c8639221f2625d9c7413648aaf50d9
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-10-19 00:21:09 +0000

    temporarily use racheles stripe model.

--- trunk/src/kip/clump/dim2/apps/Saddle2App.java
+++ trunk/src/kip/clump/dim2/apps/Saddle2App.java
@@ -11,10 +11,10 @@ import scikit.jobs.Job;
 import scikit.jobs.Simulation;
 import scikit.numerics.opt.C1Function;
 import scikit.numerics.opt.ConjugateGradient;
-import scikit.numerics.opt.Constraint;
+//import scikit.numerics.opt.Constraint;
 import scikit.numerics.opt.LinearOptimizer;
 import scikit.numerics.opt.Optimizer;
-import scikit.numerics.opt.Relaxation;
+//import scikit.numerics.opt.Relaxation;
 import scikit.params.ChoiceValue;
 import scikit.util.DoubleArray;
 import scikit.util.Pair;
@@ -50,7 +50,8 @@ public class Saddle2App extends Simulation {
 		if (params.sget("Zoom").equals("Yes"))
 			grid.setAutoScale();
 		else
-			grid.setScale(0.2, 4);
+//			grid.setScale(0.2, 4);
+			grid.setScale(-1, 1);
 		grid.registerData(clump.numColumns(), clump.numColumns(), clump.coarseGrained());
 
 //		plot.registerLines("", new PointSet(0, 1, section), Color.BLUE);
@@ -63,17 +64,22 @@ public class Saddle2App extends Simulation {
 	
 	public void run() {
 		clump = new FieldClump2D(params);
-		clump.initializeFieldWithSeed();
+		for (int i = 0 ; i < clump.phi.length; i++)
+			clump.phi[i] = 0.1*(Math.random()-1);
+//		clump.initializeFieldWithSeed();
 		
 		C1Function f = new C1Function() {
 			double[] grad = new double[clump.phi.length];
 			double freeEnergy(double p, double pb) {
 				double inf = Double.POSITIVE_INFINITY;
-				double f = 0.5*p*pb+clump.T*p*log(p);
-				return p <= 0 ? inf : f; 
+//				double f = 0.5*p*pb+clump.T*p*log(p);
+//				return p <= 0 ? inf : f; 
+				double f = 0.5*p*pb+0.5*clump.T*((1+p)*log(1+p)+(1-p)*log(1-p));
+				return (p <= -1 || p >= 1) ? inf : f;
 			}
 			double delFreeEnergy(double p, double pb) {
-				return pb+clump.T*log(p);
+//				return pb+clump.T*log(p);
+				return pb+0.5*clump.T*(log(1+p) - log(1-p));
 			}
 			public Pair<Double,double[]> calculate(final double[] p) {
 				final double[] pb = clump.phi_bar;
@@ -91,25 +97,25 @@ public class Saddle2App extends Simulation {
 			}
 		};
 		
-		Constraint c = new Constraint() {
-			double[] grad = new double[clump.phi.length];
-			public double tolerance() {
-				return 0.01;
-			}
-			public Pair<Double,double[]> calculate(double[] p) {
-				double c = 0;
-				for (int i = 0; i < p.length; i++) {
-					c += (p[i]-1)*(p[i]-1) / p.length;
-					grad[i] = 2*(p[i]-1);
-				}
-				return new Pair<Double,double[]>(c, grad);
-			}
-		};
+//		Constraint c = new Constraint() {
+//			double[] grad = new double[clump.phi.length];
+//			public double tolerance() {
+//				return 0.01;
+//			}
+//			public Pair<Double,double[]> calculate(double[] p) {
+//				double c = 0;
+//				for (int i = 0; i < p.length; i++) {
+//					c += (p[i]-1)*(p[i]-1) / p.length;
+//					grad[i] = 2*(p[i]-1);
+//				}
+//				return new Pair<Double,double[]>(c, grad);
+//			}
+//		};
 		
 		Optimizer opt = new ConjugateGradient(clump.phi.length, new LinearOptimizer());
-		opt = new Relaxation(clump.phi.length, 0.1);
+//		opt = new Relaxation(clump.phi.length, 0.1);
 		opt.setFunction(f);
-		opt.addConstraint(c);
+//		opt.addConstraint(c);
 		opt.initialize(clump.phi);
 		
 		while (!opt.isFinished()) {
