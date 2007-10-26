7b762dc8b54a701f87c1486dff8675b63de28f57
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-10-26 23:27:51 +0000

    revert saddle2app.

--- trunk/src/kip/clump/dim2/apps/Saddle2App.java
+++ trunk/src/kip/clump/dim2/apps/Saddle2App.java
@@ -9,17 +9,19 @@ import scikit.graphics.dim2.Plot;
 import scikit.jobs.Control;
 import scikit.jobs.Job;
 import scikit.jobs.Simulation;
-import scikit.numerics.opt.C1Function;
-import scikit.numerics.opt.ConjugateGradient;
-//import scikit.numerics.opt.Constraint;
-import scikit.numerics.opt.LinearOptimizer;
+import scikit.numerics.fn.C1Function;
+//import scikit.numerics.opt.ConjugateGradient;
+import scikit.numerics.opt.Constraint;
+//import scikit.numerics.opt.LinearOptimizer;
 import scikit.numerics.opt.Optimizer;
-//import scikit.numerics.opt.Relaxation;
+import scikit.numerics.opt.Relaxation;
 import scikit.params.ChoiceValue;
 import scikit.util.DoubleArray;
 import scikit.util.Pair;
 
 public class Saddle2App extends Simulation {
+	final double inf = Double.POSITIVE_INFINITY;
+	
 	Grid grid = new Grid("Grid");
 	Plot plot = new Plot("");
 	FieldClump2D clump;
@@ -50,8 +52,7 @@ public class Saddle2App extends Simulation {
 		if (params.sget("Zoom").equals("Yes"))
 			grid.setAutoScale();
 		else
-//			grid.setScale(0.2, 4);
-			grid.setScale(-1, 1);
+			grid.setScale(0.2, 4);
 		grid.registerData(clump.numColumns(), clump.numColumns(), clump.coarseGrained());
 
 //		plot.registerLines("", new PointSet(0, 1, section), Color.BLUE);
@@ -64,58 +65,49 @@ public class Saddle2App extends Simulation {
 	
 	public void run() {
 		clump = new FieldClump2D(params);
-		for (int i = 0 ; i < clump.phi.length; i++)
-			clump.phi[i] = 0.1*(Math.random()-1);
-//		clump.initializeFieldWithSeed();
+		clump.initializeFieldWithSeed();
 		
 		C1Function f = new C1Function() {
 			double[] grad = new double[clump.phi.length];
-			double freeEnergy(double p, double pb) {
-				double inf = Double.POSITIVE_INFINITY;
-//				double f = 0.5*p*pb+clump.T*p*log(p);
-//				return p <= 0 ? inf : f; 
-				double f = 0.5*p*pb+0.5*clump.T*((1+p)*log(1+p)+(1-p)*log(1-p));
-				return (p <= -1 || p >= 1) ? inf : f;
-			}
-			double delFreeEnergy(double p, double pb) {
-//				return pb+clump.T*log(p);
-				return pb+0.5*clump.T*(log(1+p) - log(1-p));
-			}
 			public Pair<Double,double[]> calculate(final double[] p) {
 				final double[] pb = clump.phi_bar;
 				clump.convolveWithRange(p, pb, clump.R);
-				double f = 0;
+				double fe_acc = 0;
 				for (int i = 0; i < p.length; i++) {
-					f += freeEnergy(p[i], pb[i]) / p.length;
-					grad[i] = delFreeEnergy(p[i], pb[i]);
+					if (p[i] <= 0) {
+						fe_acc = grad[i] = inf;
+					}
+					else {
+						fe_acc += (p[i]*pb[i]/2+clump.T*p[i]*log(p[i])) / p.length;
+						grad[i] = pb[i]+clump.T*log(p[i]);
+					}
 				}
 				double mu = DoubleArray.mean(grad);
 				for (int i = 0; i < p.length; i++) {
 					grad[i] -= mu;
 				}
-				return new Pair<Double,double[]>(f, grad);
+				return new Pair<Double,double[]>(fe_acc, grad);
 			}
 		};
 		
-//		Constraint c = new Constraint() {
-//			double[] grad = new double[clump.phi.length];
-//			public double tolerance() {
-//				return 0.01;
-//			}
-//			public Pair<Double,double[]> calculate(double[] p) {
-//				double c = 0;
-//				for (int i = 0; i < p.length; i++) {
-//					c += (p[i]-1)*(p[i]-1) / p.length;
-//					grad[i] = 2*(p[i]-1);
-//				}
-//				return new Pair<Double,double[]>(c, grad);
-//			}
-//		};
-		
-		Optimizer opt = new ConjugateGradient(clump.phi.length, new LinearOptimizer());
-//		opt = new Relaxation(clump.phi.length, 0.1);
+		Constraint c = new Constraint() {
+			double[] grad = new double[clump.phi.length];
+			public double tolerance() {
+				return 0.01;
+			}
+			public Pair<Double,double[]> calculate(double[] p) {
+				double c = 0;
+				for (int i = 0; i < p.length; i++) {
+					c += (p[i]-1)*(p[i]-1) / p.length;
+					grad[i] = 2*(p[i]-1);
+				}
+				return new Pair<Double,double[]>(c, grad);
+			}
+		};
+
+		Optimizer opt = new Relaxation(clump.phi.length, 0.1);
 		opt.setFunction(f);
-//		opt.addConstraint(c);
+		opt.addConstraint(c);
 		opt.initialize(clump.phi);
 		
 		while (!opt.isFinished()) {
