384a3adbab9bb464f80dbb6ea954cc0704d55da1
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-11-01 13:49:02 +0000

    saddle2app now independent of other clump code.

--- trunk/src/kip/clump/dim2/apps/Saddle2App.java
+++ trunk/src/kip/clump/dim2/apps/Saddle2App.java
@@ -1,20 +1,24 @@
 package kip.clump.dim2.apps;
 
 import static java.lang.Math.log;
+import static kip.util.MathPlus.hypot;
+import static kip.util.MathPlus.j1;
+import static scikit.util.Utilities.format;
 import static scikit.util.Utilities.frame;
-import kip.clump.dim2.FieldClump2D;
+import kip.util.Random;
 import scikit.graphics.GrayScale;
 import scikit.graphics.dim2.Grid;
 import scikit.graphics.dim2.Plot;
 import scikit.jobs.Control;
 import scikit.jobs.Job;
 import scikit.jobs.Simulation;
+import scikit.numerics.fft.util.FFT2D;
 import scikit.numerics.fn.C1Function;
-//import scikit.numerics.opt.ConjugateGradient;
-import scikit.numerics.opt.Constraint;
-//import scikit.numerics.opt.LinearOptimizer;
+import scikit.numerics.fn.Function2D;
+import scikit.numerics.opt.ConjugateGradient;
+//import scikit.numerics.opt.Constraint;
+import scikit.numerics.opt.LinearOptimizer;
 import scikit.numerics.opt.Optimizer;
-import scikit.numerics.opt.Relaxation;
 import scikit.params.ChoiceValue;
 import scikit.util.DoubleArray;
 import scikit.util.Pair;
@@ -24,37 +28,47 @@ public class Saddle2App extends Simulation {
 	
 	Grid grid = new Grid("Grid");
 	Plot plot = new Plot("");
-	FieldClump2D clump;
+	
+	double L, R, T, dt;
+	int dim;
+	double[] phi, phibar;
+	Random random = new Random();
+	FFT2D fft;
+	double fe;
+	int time;
+	
 	
 	public static void main(String[] args) {
-		new Control(new Saddle2App(), "Clump Model Saddle Profile");
+		new Control(new Saddle2App(), "Clump Optimizer");
 	}
 
 	public Saddle2App() {
 		frame(grid, plot);
 		params.addm("Zoom", new ChoiceValue("Yes", "No"));
-		params.addm("T", 0.1);
+		params.addm("T", 0.05);
 		params.addm("dt", 1.0);
 		params.add("R", 1000.0);
 		params.add("L", 5000.0);
-		params.add("dx", 100.0);
+		params.add("dim", 32);
 		params.add("Random seed", 0);
 		params.add("Time");
 		params.add("F density");
-		params.add("dF/dphi");
-		params.add("Valid profile");
-		flags.add("Res up");
-		flags.add("Res down");
 	}
 	
 	public void animate() {
+		T = params.fget("T");
+		dt = params.fget("dt");
+		R = params.fget("R");
+		
 		grid.setColors(new GrayScale());
 		if (params.sget("Zoom").equals("Yes"))
 			grid.setAutoScale();
 		else
-			grid.setScale(0.2, 4);
-		grid.registerData(clump.numColumns(), clump.numColumns(), clump.coarseGrained());
-
+			grid.setScale(0, 4);
+		grid.registerData(dim, dim, phi);
+		
+		params.set("F density", format(fe-0.5));
+		params.set("Time", time);
 //		plot.registerLines("", new PointSet(0, 1, section), Color.BLUE);
 	}
 	
@@ -64,22 +78,36 @@ public class Saddle2App extends Simulation {
 	}
 	
 	public void run() {
-		clump = new FieldClump2D(params);
-		clump.initializeFieldWithSeed();
+		T = params.fget("T");
+		dt = params.fget("dt");
+		R = params.fget("R");
+		L = params.fget("L");
+		dim = params.iget("dim");
+		time = 0;
+		
+		phi = new double[dim*dim];
+		phibar = new double[dim*dim];
+		for (int i = 0; i < dim*dim; i++)
+			phi[i] = 1 + 0.01*random.nextGaussian();
+		
+		fft = new FFT2D(dim, dim);
+		fft.setLengths(L, L);
+		
+		random.setSeed(params.iget("Random seed"));
 		
 		C1Function f = new C1Function() {
-			double[] grad = new double[clump.phi.length];
+			double[] grad = new double[dim*dim];
 			public Pair<Double,double[]> calculate(final double[] p) {
-				final double[] pb = clump.phi_bar;
-//				clump.convolveWithRange(p, pb, clump.R);
+				final double[] pb = phibar;
+				fft.convolve(p, pb, potential);
 				double fe_acc = 0;
 				for (int i = 0; i < p.length; i++) {
 					if (p[i] <= 0) {
 						fe_acc = grad[i] = inf;
 					}
 					else {
-						fe_acc += (p[i]*pb[i]/2+clump.T*p[i]*log(p[i])) / p.length;
-						grad[i] = pb[i]+clump.T*log(p[i]);
+						fe_acc += (p[i]*pb[i]/2+T*p[i]*log(p[i])) / p.length;
+						grad[i] = pb[i]+T*log(p[i]);
 					}
 				}
 				double mu = DoubleArray.mean(grad);
@@ -90,32 +118,41 @@ public class Saddle2App extends Simulation {
 			}
 		};
 		
-		Constraint c = new Constraint() {
-			double[] grad = new double[clump.phi.length];
-			public double tolerance() {
-				return 0.01;
-			}
-			public Pair<Double,double[]> calculate(double[] p) {
-				double c = 0;
-				for (int i = 0; i < p.length; i++) {
-					c += (p[i]-1)*(p[i]-1) / p.length;
-					grad[i] = 2*(p[i]-1);
-				}
-				return new Pair<Double,double[]>(c, grad);
-			}
-		};
+//		Constraint c = new Constraint() {
+//			double[] grad = new double[dim*dim];
+//			public double tolerance() {
+//				return 0.01;
+//			}
+//			public Pair<Double,double[]> calculate(double[] p) {
+//				double c = 0;
+//				for (int i = 0; i < p.length; i++) {
+//					c += (p[i]-1)*(p[i]-1) / p.length;
+//					grad[i] = 2*(p[i]-1);
+//				}
+//				return new Pair<Double,double[]>(c, grad);
+//			}
+//		};
 
-		Optimizer opt = new Relaxation(clump.phi.length, 0.1);
+//		Optimizer opt = new Relaxation(dim*dim, dt);
+		Optimizer opt = new ConjugateGradient(dim*dim, new LinearOptimizer());
 		opt.setFunction(f);
-		opt.addConstraint(c);
-		opt.initialize(clump.phi);
+//		opt.addConstraint(c);
+		opt.initialize(phi);
 		
 		while (!opt.isFinished()) {
+//		while(true) {
 			opt.step();
-			System.out.println(f.eval(clump.phi) + " " + DoubleArray.mean(clump.phi));
+			fe = f.eval(phi);
+			time++;
 			Job.animate();
 		}
-		System.out.println("done");
+//		System.out.println("done");
 	}
 	
+	private Function2D potential = new Function2D() {
+		public double eval(double k1, double k2) {
+			double kR = hypot(k1,k2)*R;
+			return kR == 0 ? 1 : 2*j1(kR)/kR;
+		}
+	};
 }
