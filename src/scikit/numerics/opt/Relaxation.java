eaae1e6559edb6532128858d449940896f77ac05
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-11-09 22:48:36 +0000

    more work on optimization with constraints.

--- src/scikit/numerics/opt/Relaxation.java
+++ src/scikit/numerics/opt/Relaxation.java
@@ -36,35 +36,49 @@ public class Relaxation extends Optimizer {
 	 *  @return the constrained gradient of the objective function
 	 */
 	protected double[] df_constrained(double[] p) {
-		
-		ArrayList<Constraint> cs = _constraints;
-		double[][] d_c = new double[cs.size()][];
-		double[] c = new double[cs.size()];
-		for (int i = 0; i < cs.size(); i++) {
-			Pair<Double, double[]> r = cs.get(i).calculate(p);
-			c[i] = r.fst();
-			d_c[i] = r.snd();
-		}
-		double[][] m = new double[cs.size()][cs.size()];
-		for (int i = 0; i < cs.size(); i++) {
-			for (int j = i; j < cs.size(); j++) {
-				m[i][j] = m[j][i] = dot(d_c[i], d_c[j]); 
-			}
-		}
-		Matrix M = new Matrix(m);
-		
 		double[] d_f = _f.grad(p);
-		Matrix B = new Matrix(cs.size(), 1);
-		for (int i = 0; i < cs.size(); i++) {
-			double stiffness = cs.get(i).stiffness();
-			B.set(i, 0, c[i]*stiffness*p.length/dt - dot(d_f, d_c[i]));
+		ArrayList<Constraint> cs = _constraints;		
+		if (cs.size() > 0) {
+			double[][] d_c = new double[cs.size()][];
+			double[] c = new double[cs.size()];
+			for (int i = 0; i < cs.size(); i++) {
+				Pair<Double, double[]> r = cs.get(i).calculate(p);
+				c[i] = r.fst();
+				d_c[i] = r.snd();
+			}
+			double[][] m = new double[cs.size()][cs.size()];
+			for (int i = 0; i < cs.size(); i++) {
+				for (int j = i; j < cs.size(); j++) {
+					m[i][j] = m[j][i] = dot(d_c[i], d_c[j]); 
+				}
+			}
+			Matrix M = new Matrix(m);
+
+			Matrix B = new Matrix(cs.size(), 1);
+			for (int i = 0; i < cs.size(); i++) {
+				double stiffness = cs.get(i).stiffness();
+				B.set(i, 0, c[i]*stiffness*p.length/dt - dot(d_f, d_c[i]));
+			}
+			Matrix Lambda = M.solve(B);
+
+			for (int i = 0; i < cs.size(); i++)
+				for (int x = 0; x < p.length; x++)
+					d_f[x] += Lambda.get(i, 0) * d_c[i][x];
 		}
-		Matrix Lambda = M.solve(B);
-		
-		for (int i = 0; i < cs.size(); i++)
-			for (int x = 0; x < p.length; x++)
-				d_f[x] += Lambda.get(i, 0) * d_c[i][x];
-		
 		return d_f;
+	}
+	
+	public double dc_dt(double[] p, Constraint c) {
+		ArrayList<Constraint> cs = _constraints;
+		if (!cs.contains(c)) {
+			throw new IllegalArgumentException("Constraint not contained");				
+		}
+		else {
+			cs.remove(c);
+			double[] d_f = df_constrained(p);
+			double[] d_c = c.grad(p);
+			cs.add(c);
+			return -dot(d_f, d_c)/p.length;
+		}
 	}
 }
