96ee3d486e19ca5ad5c416f00dfb17fe5d20749a
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2007-11-30 20:50:50 +0000

    fix bug in grid3dview, with slider view parameter.

--- src/scikit/graphics/dim3/Grid3DView.java
+++ src/scikit/graphics/dim3/Grid3DView.java
@@ -4,6 +4,16 @@ import scikit.numerics.vecmath.Quat4d;
 import scikit.numerics.vecmath.Vector3d;
 
 abstract public class Grid3DView {
+	private double _displayParam = 0.5;
+
+	public double getDisplayParam() {
+		return _displayParam; 
+	}
+	
+	public void setDisplayParam(double dp) {
+		_displayParam = dp;
+	}
+	
 	// normal vectors for each cube panel
 	protected static final Vector3d[] _normal = new Vector3d[] {
 		new Vector3d(-1, 0, 0),
@@ -49,6 +59,4 @@ abstract public class Grid3DView {
 	
 	abstract public void draw(Gfx3D g);
 	abstract public void rotateStructure(Quat4d q);
-	abstract public void setDisplayParam(double x);
-	abstract public double getDisplayParam();
 }
