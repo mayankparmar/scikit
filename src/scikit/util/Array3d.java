f97b58607f24a443c19deeba43485355d6034941
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2008-01-16 00:45:55 +0000

    Expand console functionality.

---
+++ src/scikit/util/Array3d.java
@@ -0,0 +1,117 @@
+package scikit.util;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.IOException;
+
+import kip.util.MathPlus;
+
+import scikit.numerics.fn.Function3D;
+
+public class Array3d implements Cloneable {
+	private double[] _a;
+	private int _nx, _ny, _nz;
+	private double _lx, _ly, _lz;
+	
+	
+	public Array3d(int nx, int ny, int nz) {
+		_lx = _nx = nx;
+		_ly = _ny = ny;
+		_lz = _nz = nz;
+		_a = new double[_nx*_ny*_nz];
+	}
+	
+	public Array3d(File file) {
+		try {
+			DataInputStream dis = FileUtil.disFromString(file.toString());
+			_lx = _nx = dis.readInt();
+			_ly = _ny = dis.readInt();
+			_lz = _nz = dis.readInt();
+			_a = new double[_nx*_ny*_nz];
+			for (int i = 0; i < _nx*_ny*_nz; i++)
+				_a[i] = dis.readDouble();
+			dis.close();
+		} catch(IOException e) {
+			System.err.println("Could not read file: " + file);
+		}
+	}
+	
+	public Array3d clone() {
+		try {
+			Array3d ret = (Array3d)super.clone();
+			ret._a = DoubleArray.clone(_a);
+			return ret;
+		} catch (CloneNotSupportedException e) {
+			return null;
+		}
+	}
+	
+	public void setLengths(double lx, double ly, double lz) {
+		_lx = lx;
+		_ly = ly;
+		_lz = lz;
+	}
+	
+	public int nx() { return _nx; }
+	public int ny() { return _ny; }
+	public int nz() { return _nz; }
+	public double lx() { return _lx; }
+	public double ly() { return _ly; }
+	public double lz() { return _lz; }
+	public double[] array() { return _a; }
+
+	public double get(int x, int y, int z) {
+		return _a[_nx*_ny*z+_nx*y+x];
+	}
+	
+	public void set(int x, int y, int z, double v) {
+		_a[_nx*_ny*z+_nx*y+x] = v;
+	}
+	
+	public void build(Function3D f) {
+		double dx = _lx/_nx;
+		double dy = _ly/_ny;
+		double dz = _lz/_nz;
+		for (int k = 0; k < _nz; k++)
+			for (int j = 0; j < _nx; j++)
+				for (int i = 0; i < _ny; i++)
+					set(i, j, k, f.eval(i*dx, j*dy, k*dz));
+	}
+	
+	public void buildShell(final double k, final double width) {
+		build(new Function3D() {
+			public double eval(double x, double y, double z) {
+				x = Math.min(x, _lx-x);
+				y = Math.min(y, _ly-y);
+				z = Math.min(z, _lz-z);
+				double r = MathPlus.hypot(x,y,z);
+				return (Math.abs(k-r) < width/2.) ? 1 : 0;
+			}
+		});
+	}
+	
+	public void shift(double x, double y, double z) {
+		int xs = (int)Math.round(x/(_lx/_nx));
+		int ys = (int)Math.round(y/(_ly/_ny));
+		int zs = (int)Math.round(z/(_lz/_nz));
+		Array3d scratch = clone();
+		for (int k = 0; k < _nz; k++) {
+			int kp = (k+zs+_nz)%_nz;
+			for (int j = 0; j < _ny; j++) { 
+				int jp = (j+ys+_ny)%_ny;
+				for (int i = 0; i < _nx; i++) {
+					int ip = (i+xs+_nx)%_nx;
+					scratch.set(i, j, k, get(ip, jp, kp));
+				}
+			}
+		}
+		DoubleArray.copy(scratch._a, _a);
+	}
+	
+	public double[] sliceX(int y, int z) {
+		double[] ret = new double[_nx];
+		for (int i = 0; i < _nx; i++)
+			ret[i] = get(i, y, z);
+		return ret;
+	}
+}
--- src/scikit/util/Commands.java
+++ src/scikit/util/Commands.java
@@ -1,20 +1,36 @@
 package scikit.util;
 
-import java.awt.Color;
+import static java.lang.Math.PI;
 
-import bsh.ClassPathException;
-import bsh.util.ClassBrowser;
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JDialog;
 
 import scikit.graphics.dim2.Plot;
+import scikit.graphics.dim3.Grid3D;
+import scikit.numerics.fft.util.FFT3D;
+import bsh.ClassPathException;
+import bsh.util.ClassBrowser;
 
 public class Commands {
 	
 	// utility method for quickly viewing data.
 	public static void plot(double[] data) {
-		Plot plot = new Plot("Debug plot");
+		Plot plot = new Plot("Quick Plot");
 		plot.registerPoints("", new scikit.dataset.PointSet(0, 1, data), Color.BLUE);
 		Utilities.frame(plot);
-//		Thread.yield();
+	}
+	
+	public static void grid3d(int w, int h, int d, double[] data) {
+		Grid3D grid = new Grid3D("Quick Grid");
+		grid.registerData(w, h, d, data);
+		Utilities.frame(grid);
+	}
+	
+	public static void grid3d(Array3d a) {
+		grid3d(a.nx(), a.ny(), a.nz(), a.array());
 	}
 	
 	public static void classBrowser() { 
@@ -23,5 +39,29 @@ public class Commands {
 			browser.init();
 		} catch(ClassPathException e) {}
 		Utilities.frame(browser, "Class browser");
+	}
+	
+	public static Array3d load3d() {
+		try {
+			String fname = FileUtil.loadDialog(new JDialog(), "");
+			if (fname != null)
+				return new Array3d(new File(fname));
+		} catch(IOException e) {}
+		return null;
+	}
+	
+	public static Array3d fftReal(Array3d a) {		
+		FFT3D fft = new FFT3D(a.nx(), a.ny(), a.nz());
+		fft.setLengths(a.lx(), a.ly(), a.lz());
+		fft.transform(a.array(), fft.getScratch());
+
+		Array3d ret = new Array3d(a.nx(), a.ny(), a.nz());
+		double dx = a.lx()/a.nx();
+		double dy = a.ly()/a.ny();
+		double dz = a.lz()/a.nz();
+		ret.setLengths(2*PI/dx, 2*PI/dy, 2*PI/dz);
+		for (int i = 0; i < ret.array().length; i++)
+			ret.array()[i] = fft.getScratch()[2*i+0];
+		return ret;
 	}
 }
