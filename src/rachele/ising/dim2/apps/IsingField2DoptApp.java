112d028172c40689e1b02fed935b3e95c4e2ba6a
erg.dominguez <erg.dominguez@508e2490-7634-0410-858c-2f298a935b86>
2008-02-01 20:28:24 +0000

    2D ising field program has a new slider to adjust the noise and an option to use the half step method instead of the slow method

--- src/rachele/ising/dim2/apps/IsingField2DoptApp.java
+++ src/rachele/ising/dim2/apps/IsingField2DoptApp.java
@@ -3,8 +3,9 @@ package rachele.ising.dim2.apps;
 
 import static java.lang.Math.floor;
 import static scikit.util.Utilities.*;
-import rachele.ising.dim2.IsingField2D;
+import rachele.ising.dim2.IsingField2Dopt;
 import rachele.ising.dim2.StructureFactor;
+//import rachele.util.FileUtil;
 import scikit.graphics.dim2.Grid;
 import scikit.jobs.Control;
 import scikit.jobs.Job;
@@ -16,8 +17,9 @@ public class IsingField2DoptApp extends Simulation{
     Grid grid = new Grid("Phi(x)");
     Grid sfGrid = new Grid("S(k)");
 	StructureFactor sf;
-    IsingField2D ising;
-
+    IsingField2Dopt ising;
+	boolean initFile = false;
+    
     public static void main(String[] args) {
 		new Control(new IsingField2DoptApp(), "Ising Field");
 	}
@@ -31,24 +33,26 @@ public class IsingField2DoptApp extends Simulation{
 		params.addm("Plot FEvT", new ChoiceValue("Off", "On"));
 		params.addm("Horizontal Slice", new DoubleValue(0.5, 0, 0.9999).withSlider());
 		params.addm("Vertical Slice", new DoubleValue(0.5, 0, 0.9999).withSlider());
-//		params.addm("kR", new DoubleValue(5.135622302, 0.0, 6.0).withSlider());
 		params.addm("T", 0.09);
 		params.addm("H", 0.0);
 		params.addm("tolerance", 0.0001);
 		params.addm("dt", 1.0);
 		params.addm("J", -1.0);
 		params.addm("R", 1000000.0);
-		params.add("L/R", 5.0);
+		params.add("L/R", 10.0);
 		params.add("R/dx", 16.0);
 		params.add("kR bin-width", 0.1);
 		params.add("Random seed", 0);
 		params.add("Magnetization", 0.7);
 		params.add("Time");
-		params.add("Lp");
-
+		//params.add("Lp");
+		params.add("vert sf");
+		params.add("hor sf");
+		
 		flags.add("Write Config");
 		flags.add("Clear");
 		flags.add("SF");
+
 	}
 	
 	public void animate() {
@@ -59,15 +63,17 @@ public class IsingField2DoptApp extends Simulation{
 			grid.setScale(-1, 1);
 		sfGrid.registerData(ising.Lp, ising.Lp, sf.sFactor);
 		grid.registerData(ising.Lp, ising.Lp, ising.phi);
+		params.set("vert sf", sf.peakValueV());
+		params.set("hor sf", sf.peakValueH());
 	}
 	
 	public void clear() {
 	}
 	
 	public void run() {
-		ising = new IsingField2D(params);
+		ising = new IsingField2Dopt(params);
 		double binWidth = params.fget("kR bin-width");
-		binWidth = IsingField2D.KR_SP / floor(IsingField2D.KR_SP/binWidth);
+		binWidth = IsingField2Dopt.KR_SP / floor(IsingField2Dopt.KR_SP/binWidth);
         sf = new StructureFactor(ising.Lp, ising.L, ising.R, binWidth, ising.dt);
 		sf.setBounds(0.1, 14);
 		
@@ -75,9 +81,45 @@ public class IsingField2DoptApp extends Simulation{
         	ising.readParams(params);
 			params.set("Time", ising.time());
 			ising.simulate();
-			sf.accumulateAll(ising.time(), ising.coarseGrained());
+			//writeDataToFile();
+			//sf.accumulateAll(ising.time(), ising.coarseGrained());
 			Job.animate();
 		}
  	}
+//	public void writeDataToFile(){
+//		boolean SvH = false;
+//		if (params.sget("Interaction")=="Square"){
+//				String dataFileV = "../../../research/javaData/sfData/dataFileV";
+//				String dataFileH = "../../../research/javaData/sfData/dataFileH";
+//				if (initFile == false){
+//					initFile(dataFileV, SvH);
+//					initFile(dataFileH, SvH);
+//					initFile = true;
+//				}
+//				if (SvH){
+//					FileUtil.printlnToFile(dataFileH, ising.H, sf.peakValueH(), ising.freeEnergy, ising.time());
+//					FileUtil.printlnToFile(dataFileV, ising.H, sf.peakValueV(), ising.freeEnergy, ising.time());					
+//				}else{
+//					FileUtil.printlnToFile(dataFileH, ising.T, sf.peakValueH(), ising.freeEnergy, ising.time());
+//					FileUtil.printlnToFile(dataFileV, ising.T, sf.peakValueV(), ising.freeEnergy, ising.time());
+//				}			
+//				System.out.println("Data written to file for time = " + ising.time());
+//		}else if(params.sget("Interaction")== "Circle"){
+//			String dataStripe = "../../../research/javaData/sfData/dataStripe";
+//			String dataClump = "../../../research/javaData/sfData/dataClump";
+//			if (initFile == false){
+//				initFile(dataStripe, SvH);
+//				initFile(dataClump, SvH);
+//				initFile = true;
+//			}
+//			if(SvH){
+//				FileUtil.printlnToFile(dataClump, ising.H, sf.peakValueC(), ising.freeEnergy, ising.time());
+//				FileUtil.printlnToFile(dataStripe, ising.H, sf.peakValueS(), ising.freeEnergy, ising.time());					
+//			}else{
+//				FileUtil.printlnToFile(dataClump, ising.T, sf.peakValueC(), ising.freeEnergy, ising.time());
+//				FileUtil.printlnToFile(dataStripe, ising.T, sf.peakValueS(), ising.freeEnergy, ising.time());
+//			}
+//			System.out.println("Data written to file for time = " + ising.time());
+//		}
 	
 }
