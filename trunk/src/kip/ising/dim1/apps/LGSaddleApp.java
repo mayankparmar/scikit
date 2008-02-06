aec2288df84ddb193b27259ccb6a917d68337f7e
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2008-02-06 23:05:38 +0000

    add landau ginsburg saddle point finder.

---
+++ trunk/src/kip/ising/dim1/apps/LGSaddleApp.java
@@ -0,0 +1,69 @@
+package kip.ising.dim1.apps;
+
+import static java.lang.Math.pow;
+
+import java.awt.Color;
+
+import scikit.dataset.PointSet;
+import scikit.graphics.dim2.Plot;
+import scikit.jobs.Control;
+import scikit.jobs.Job;
+import scikit.jobs.Simulation;
+import scikit.jobs.params.DoubleValue;
+
+public class LGSaddleApp extends Simulation{
+	Plot profile = new Plot("Droplet profile");
+	Plot free = new Plot("Free energy");
+	double[] fe;
+	double[] xs; 
+	
+	public static void main (String[] args) {
+		new Control(new LGSaddleApp(), "Landau Ginzburg Saddle");
+	}
+
+	public void load(Control c) {
+		c.frame(profile, free);
+		profile.setAutoScale(true);
+		free.setAutoScale(true);
+		params.addm("phi0", new DoubleValue(-0.1, -20, 0).withSlider());
+		params.addm("eps", 0.01);
+		params.addm("dim", 8);
+		params.addm("dt", 0.001);
+		params.add("len", 10000);
+	}
+	
+	double freeEnergy(double x, double v, double eps) {
+		return v*v/2 + x*x*x/3 - eps*x*x/2;
+	}
+	
+	public void animate() {
+		double eps = params.fget("eps");
+		double dim = params.fget("dim");
+		double dt = params.fget("dt");
+		
+		double x = params.fget("phi0");
+		double v = 0;
+		for (int i = 0; i < xs.length; i++) {
+			double t = (i+1)*dt;
+			double a = -((dim-1)/t)*v + x*x - eps*x;
+			v += a*dt; 
+			x += v*dt;
+			xs[i] = x;
+			fe[i] = dt*(freeEnergy(x, v, eps)-freeEnergy(eps,0, eps))*pow(t, dim-1);
+		}
+		
+		profile.registerLines("profile", new PointSet(0, dt, xs), Color.BLACK);
+		free.registerLines("free", new PointSet(0, dt, fe), Color.RED);
+	}
+
+	public void run() {
+		xs = new double[params.iget("len")];
+		fe = new double[xs.length];
+		
+		while (true)
+			Job.animate();
+	}
+	
+	public void clear() {
+	}
+}
