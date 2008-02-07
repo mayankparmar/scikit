fdefbd1dd2175f2c87bd2d81b6477259b6060191
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2008-02-07 21:32:11 +0000

    Ginsburg Landau saddle finder now prints free energy. Notice divergence as d->6!

--- src/kip/ising/dim1/apps/LGSaddleApp.java
+++ src/kip/ising/dim1/apps/LGSaddleApp.java
@@ -11,6 +11,7 @@ import scikit.jobs.Control;
 import scikit.jobs.Job;
 import scikit.jobs.Simulation;
 import scikit.jobs.params.DoubleValue;
+import scikit.util.Utilities;
 
 public class LGSaddleApp extends Simulation{
 	Plot profile = new Plot("Droplet profile");
@@ -28,11 +29,12 @@ public class LGSaddleApp extends Simulation{
 		c.frame(profile, free);
 		profile.setAutoScale(true);
 		free.setAutoScale(true);
-		params.addm("phi0", new DoubleValue(-0.03196, -1, 0).withSlider());
-		params.addm("eps", 0.01);
-		params.addm("dim", 3);
-		params.addm("dt", 0.01);
-		params.add("len", 10000);
+		params.addm("phi0", new DoubleValue(-0.03196, -1000, 0).withSlider());
+		params.addm("eps", 1);
+		params.addm("dim", 5.6);
+		params.addm("dt", 0.0001);
+		params.add("len", 100000);
+		params.add("free energy");
 	}
 	
 	double freeEnergy(double x, double v, double eps) {
@@ -43,6 +45,7 @@ public class LGSaddleApp extends Simulation{
 		double eps = params.fget("eps");
 		double dim = params.fget("dim");
 		double dt = params.fget("dt");
+		double fe_net = 0;
 		
 		double x = params.fget("phi0");
 		double v = 0;
@@ -51,13 +54,15 @@ public class LGSaddleApp extends Simulation{
 			double a = -((dim-1)/t)*v + x*x - eps*x;
 			v += a*dt; 
 			x += v*dt;
-			x = Math.min(x, 10);
+			x = Math.min(x, 1000);
 			xs[i] = x/eps;
 			fe[i] = dt*(freeEnergy(x, v, eps)-freeEnergy(eps,0, eps))*pow(t, dim-1);
+			fe_net += fe[i];
 		}
 		
 		profile.registerLines("profile", new PointSet(0, dt*sqrt(eps), xs), Color.BLACK);
 		free.registerLines("free", new PointSet(0, dt, fe), Color.RED);
+		params.set("free energy", Utilities.format(fe_net));
 	}
 
 	public void run() {
