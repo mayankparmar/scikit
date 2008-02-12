9c6361fd2a977ce126d71b99ce323de9f0b5b0e1
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2008-02-12 04:45:42 +0000

    Radial saddle finder now compares to theory. Great results in 3d so far.

--- src/kip/clump/radial/ClumpRadialApp.java
+++ src/kip/clump/radial/ClumpRadialApp.java
@@ -1,9 +1,16 @@
 package kip.clump.radial;
 
-import static scikit.util.Utilities.*;
+import static java.lang.Math.PI;
+import static java.lang.Math.exp;
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+import static scikit.numerics.Math2.j0;
+import static scikit.util.Utilities.format;
+import static scikit.util.Utilities.frame;
 
 import java.awt.Color;
 
+import scikit.dataset.Function;
 import scikit.dataset.PointSet;
 import scikit.graphics.dim2.Plot;
 import scikit.jobs.Control;
@@ -23,24 +30,42 @@ public class ClumpRadialApp extends Simulation {
 		frame(plot);
 		params.addm("Saddle", new ChoiceValue("Yes", "No"));
 		params.add("Dimension", new ChoiceValue("3", "2"));
-		params.addm("T", 0.09);
+		params.addm("T", 0.0865);
 		params.addm("dt", 0.5);
-		params.add("R", 1000.);
-		params.add("L", 40000.);
-		params.add("dx", 20.);
+		params.add("R", 1.);
+		params.add("L", 40.);
+		params.add("dx", 0.01);
+		params.add("eps");
 		params.add("time");
+	}
+	
+	public Function theory() {
+		final int dim = params.iget("Dimension");
+		final double eps = clump.reducedTemperature();
+		final double sig = 1/sqrt(2);
+		final double k0 = clump.KR_SP(); 
+		return new Function(0.001, 10) {
+			public double eval(double x) {
+				if (dim == 2)
+					return 1+2*sqrt(eps)*sig*k0*j0(k0*x)*exp(-x*sqrt(eps)/sig);
+				else
+					return 1+(8/(sqrt(3)*PI))*sqrt(eps)*sig*k0*sin(k0*x)/(k0*x)*exp(-x*sqrt(eps)/sig);
+			};	
+		};
 	}
 	
 	public void animate() {
 		clump.readParams(params);
 		plot.registerLines("phi", new PointSet(0, clump.dx, clump.phi), Color.BLACK);
 		plot.registerLines("bar", new PointSet(0, clump.dx, clump.phibar), Color.RED);
+		plot.registerLines("theory", theory(), Color.BLUE);
 		
 		// clump.convolveWithRange(clump.phi, clump.phibar, clump.R);
 		// double[] temp = new double[clump.phi.length];
 		// clump.convolveWithRangeSlow(clump.phi, temp, clump.R);
 		// plot.registerLines("bar2", new PointSet(0, clump.dx, temp), Color.BLUE);
 		
+		params.set("eps", format(clump.reducedTemperature()));
 		params.set("time", format(clump.t));
 	}
 	
