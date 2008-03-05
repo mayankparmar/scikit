650aa0f87f6d84b0532f7f11379f283ed35e1a94
kbarros@gmail.com <kbarros@gmail.com@508e2490-7634-0410-858c-2f298a935b86>
2008-03-05 18:39:00 +0000

    Commands methods return newly created plots and grids

--- trunk/src/scikit/util/Commands.java
+++ trunk/src/scikit/util/Commands.java
@@ -23,55 +23,61 @@ public class Commands {
 	private static int _plotCnt = 0;
 	private static Color[] _plotColors = new Color[] {Color.BLUE, Color.RED, Color.BLACK, Color.GREEN, Color.CYAN, Color.PINK}; 
 	
-	public static void plot(DataSet data, String name) {
+	public static Plot plot(DataSet data, String name) {
 		Plot plot = new Plot("Plot");
 		plot.registerPoints(name, data, _plotColors[0]);
 		_lastPlotFrame = Utilities.frame(plot.getComponent(), plot.getTitle());
 		_lastPlot = plot;
 		_plotCnt = 1;
+		return plot;
 	}
-	public static void plot(DataSet data) {
-		plot(data, "data");
+	public static Plot plot(DataSet data) {
+		return plot(data, "data");
 	}
-	public static void replot(DataSet data, String name) {
+	public static Plot replot(DataSet data, String name) {
 		if (_lastPlot == null) {
-			plot(data, name);
+			return plot(data, name);
 		}
 		else {
 			Color c = _plotColors[_plotCnt % _plotColors.length];
 			_lastPlot.registerPoints(name, data, c);
 			_lastPlotFrame.setVisible(true);
 			_plotCnt++;
+			return _lastPlot;
 		}
 	}
-	public static void replot(DataSet data) {
-		replot(data, "data"+_plotCnt);
+	public static Plot replot(DataSet data) {
+		return replot(data, "data"+_plotCnt);
 	}
-	public static void plot(double[] data) {
-		plot(new PointSet(0, 1, DoubleArray.clone(data)));
+	public static Plot plot(double[] data) {
+		return plot(new PointSet(0, 1, DoubleArray.clone(data)));
 	}
-	public static void replot(double[] data) {
-		replot(new PointSet(0, 1, DoubleArray.clone(data)));
+	public static Plot replot(double[] data) {
+		return replot(new PointSet(0, 1, DoubleArray.clone(data)));
 	}
 
-	public static void grid(int w, int h, double[] data) {
+	public static Grid grid(int w, int h, double[] data) {
 		Grid grid = new Grid("Grid");
 		grid.registerData(w, h, data);
 		Utilities.frame(grid);
+		return grid;
 	}
 	
-	public static void grid3d() {
-		Utilities.frame(new Grid3D("Grid"));
+	public static Grid3D grid3d() {
+		Grid3D grid = new Grid3D("Grid");
+		Utilities.frame(grid);
+		return grid;
 	}
 	
-	public static void grid3d(int w, int h, int d, double[] data) {
+	public static Grid3D grid3d(int w, int h, int d, double[] data) {
 		Grid3D grid = new Grid3D("Grid");
 		grid.registerData(w, h, d, data);
 		Utilities.frame(grid);
+		return grid;
 	}
 	
-	public static void grid3d(Array3d a) {
-		grid3d(a.nx(), a.ny(), a.nz(), a.array());
+	public static Grid3D grid3d(Array3d a) {
+		return grid3d(a.nx(), a.ny(), a.nz(), a.array());
 	}
 	
 	public static void classBrowser() { 
@@ -80,6 +86,13 @@ public class Commands {
 			browser.init();
 		} catch(ClassPathException e) {}
 		Utilities.frame(browser, "Class browser");
+	}
+	
+	public static File selectDir() {
+		try {
+			return FileUtil.directoryDialog("Select a directory");
+		} catch (IOException e) {}
+		return null;
 	}
 	
 	public static Array3d load3d() {
