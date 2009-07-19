f559f815ba41882ba9c811f7e65241480fc03990
kbarros <kbarros@508e2490-7634-0410-858c-2f298a935b86>
2009-07-19 23:34:06 +0000

    nice parameters for isotropic ordering

--- src/scikit/numerics/fft/FFT2D.java
+++ src/scikit/numerics/fft/FFT2D.java
@@ -86,6 +86,44 @@ public class FFT2D {
 		}
 	}
 	
+	public void convolve(double[] src, double[] dst, double[] fn) {
+		double L1 = dim1*dx1;
+		double L2 = dim2*dx2;
+		for (int i = dim1*dim2-1; i >= 0; i--) {
+			scratch[2*i+0] = src[i]*dx1*dx2;
+			scratch[2*i+1] = 0;
+		}
+		
+		fft.transform(scratch);
+		scratch = fft.toWraparoundOrder(scratch);
+		
+		for (int i = 0; i < dim1*dim2; i++) {
+			scratch[2*i+0] *= fn[i];
+			scratch[2*i+1] *= fn[i];
+		}
+		
+		fft.backtransform(scratch);
+		for (int i = 0; i < dim1*dim2; i++) {
+			dst[i] = scratch[2*i+0] / (L1*L2);
+		}
+	}
+	
+	
+	public double[] buildFourierArray(Function2D fn) {
+		double[] ret = new double[dim1*dim2];
+		double L1 = dim1*dx1;
+		double L2 = dim2*dx2;
+		for (int x2 = -dim2/2; x2 < dim2/2; x2++) {
+			for (int x1 = -dim1/2; x1 < dim1/2; x1++) {
+				int i = dim1*((x2+dim2)%dim2) + (x1+dim1)%dim1;
+				double k1 = 2*PI*x1/L1;
+				double k2 = 2*PI*x2/L2;
+				ret[i] = fn.eval(k1, k2);
+			}
+		}
+		return ret;
+	}
+	
 	public double[] getScratch() {
 		return scratch;
 	}
